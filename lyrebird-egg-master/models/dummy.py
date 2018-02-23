bc22fa78e9f078f1ff2864295681145b516f2d66
tristantoupin <tristantoupin@gmail.com>
2018-02-22 22:40:17 -0500

    model trained - best:v1

--- lyrebird-egg-master/models/dummy.py
+++ lyrebird-egg-master/models/dummy.py
@@ -2,148 +2,60 @@ import numpy as np
 import pandas as pd
 import tensorflow as tf
 from tqdm import tqdm_notebook
+from part_1 import *
+from numpy import random
+from utils import plot_stroke
+# from part_2 import *
+# from part_3 import *
 
 strokes = np.load('../data/strokes.npy')
+strings_path = "../data/sentenses.txt"
 stroke = strokes[0]
 
-epochs = 100
-learning_rate = 0.001
-batch_size = 10
-hidden_layer_size = 256
-number_of_layers = 1
-dropout_rate = 0.5
-number_of_classes = 3
-gradient_clip_margin = 4
-window_size = 50
 
-def window_data(data, window_size):
-    X = []
-    y = []
-    
-    i = 0
-    while (i + window_size) <= len(data) - 1:
-        X.append(data[i:i+window_size])
-        y.append(data[i+window_size])
-        i += 1
-
-    assert len(X) ==  len(y)
-    return X, y
-
-
-def LSTM_cell(hidden_layer_size, batch_size, num_layers, dropout_rate = 0.5):
-    layer = tf.contrib.rnn.BasicLSTMCell(hidden_layer_size)
-    layer = tf.contrib.rnn.DropoutWrapper(layer, output_keep_prob = dropout_rate)
-                                          
-    cell = tf.contrib.rnn.MultiRNNCell([layer] * num_layers)
-    init_state = cell.zero_state(batch_size, tf.float32)
-    return cell, init_state
-                      
-                                          
-def output_layer(lstm_output, in_size, out_size):
-    data = lstm_output[:, -1, :]
-    
-    weights = tf.Variable(tf.truncated_normal([in_size, out_size], stddev = 0.05), name = 'output_layer_weights')
-    bias = tf.Variable(tf.zeros([out_size]), name = 'output_layer_bias')
-                                          
-    output = tf.matmul(data, weights) + bias
-    return output
-                                          
-
-def optimize_loss(logits, targets, learning_rate, grad_clip_margin):
-    losses = []
-    for i in range(targets.get_shape()[0]):
-        losses.append([(tf.pow(logits[i] - targets[i], 2))])
-        
-    loss = tf.reduce_sum(losses)/(2*batch_size)
-    
-    #Cliping the gradient loss
-    gradients = tf.gradients(loss, tf.trainable_variables())
-    clipper_, _ = tf.clip_by_global_norm(gradients, grad_clip_margin)
-    optimizer = tf.train.AdamOptimizer(learning_rate)
-    train_optimizer = optimizer.apply_gradients(zip(gradients, tf.trainable_variables()))
-    return loss, train_optimizer
-
-                                          
-class recurrent_neural_net(object):
-    def __init__(self):
-    
-        self.inputs = tf.placeholder(tf.float32, [batch_size, window_size, 3], name='input_data')
-        self.targets = tf.placeholder(tf.float32, [batch_size, 3], name='targets')
-        
-        cell, init_state = LSTM_cell(hidden_layer_size, batch_size, number_of_layers, dropout_rate)
-        outputs, states = tf.nn.dynamic_rnn(cell, self.inputs, initial_state=init_state)
-        self.logits = output_layer(outputs, hidden_layer_size, number_of_classes)
-        self.loss, self.opt = optimize_loss(self.logits, self.targets, learning_rate, gradient_clip_margin)
-    
-    
-tf.reset_default_graph()
-model = recurrent_neural_net()
-
-session =  tf.Session()
-session.run(tf.global_variables_initializer())
-
-
-def train(X_train, y_train):
-
-    for i in tqdm_notebook(range(epochs)):
-        traind_scores = []
-        ii = 0
-        epoch_loss = []
-        while(ii + batch_size) <= len(X_train):
-            X_batch = X_train[ii:ii+batch_size]
-            y_batch = y_train[ii:ii+batch_size]
-            o, c, _ = session.run([model.logits, model.loss, model.opt], feed_dict={model.inputs:X_batch, model.targets:y_batch})
-
-            epoch_loss.append(c)
-            traind_scores.append(o)
-            ii += batch_size
-        if (i % 10) == 0:
-            print('Epoch {}/{}'.format(i, epochs), ' Current loss: {}'.format(np.mean(epoch_loss)))
-
-def test(X_test):       
-    tests = []
-    i = 0
-    while i+batch_size <= len(X_test):
-        o = session.run([model.logits], feed_dict={model.inputs:X_test[i:i+batch_size]})
-        i += batch_size
-        tests.append(o)
-    return tests
 
 def generate_unconditionally(random_seed=1):
     # Input:
     #   random_seed - integer
-    X, y = window_data(strokes[0], window_size)
-
-    X_train  = np.array(X[:100])
-    y_train = np.array(y[:100])
-
-    X_test = np.array(X[:100])
-    y_test = np.array(y[:100])
-
-    print("X_train size: {}".format(X_train.shape))
-    print("y_train size: {}".format(y_train.shape))
-    print("X_test size: {}".format(X_test.shape))
-    print("y_test size: {}".format(y_test.shape))
-
-    train(X_train, y_train)
+    prep_model("model_v1")
     
-
-    temp_test = np.asarray(test(X_test))
-    temp_test = (temp_test).reshape(len(X_test),3)
-
-
-    for count, item in enumerate(temp_test):
-        if item[0] < 0.5:
-            temp_test[count][0] = 0
-        else:
-            temp_test[count][0] = 1
-    print(temp_test.shape)
-    print(temp_test)
+    X_train, X_test, y_train, y_test = slipt_data_part_1(strokes, random_seed, 0.8, 80)
     
-    
+    print("X_train shape:", X_train.shape)
+    print("X_test shape:", X_test.shape)
+    print("y_train shape:", y_train.shape)
+    print("y_test shape:", y_test.shape)
+    train(X_train, y_train, "model_256_b-100")
+
+    test_output, output_start = test(X_test)
+    print("Example")
+    plot_stroke(y_test[output_start:output_start+700])
+    print("Generated")
+
     # Output:
     #   stroke - numpy 2D-array (T x 3)
-    return (temp_test)
+    return (test_output)
+
+
+def prep_data_part2(train_ratio, max_train_size = float("inf")):
+    # the variable max_train_size, was needed in order to reduce the training data set (time=
+    data = ""
+    with open ("strings_path.txt", "r") as myfile:
+        data = myfile.readlines()
+        
+    assert len(data) ==  len(strokes)
+    
+    train_length = min(max_train_size, int(train_ratio * len(data)))
+    
+    X_train = data[:train_length]
+    X_test = data[train_length:]
+    y_train = data[:train_length]
+    y_test = data[train_length:]
+    
+    return X_train, X_test, y_train, y_test
+
+    
+
 
 
 def generate_conditionally(text='welcome to lyrebird', random_seed=1):
@@ -151,6 +63,7 @@ def generate_conditionally(text='welcome to lyrebird', random_seed=1):
     #   text - str
     #   random_seed - integer
 
+    
     # Output:
     #   stroke - numpy 2D-array (T x 3)
     return stroke
---
+++ lyrebird-egg-master/models/part_1.py
@@ -0,0 +1,146 @@
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+from tqdm import tqdm_notebook
+from numpy import random
+
+epochs = 80
+learning_rate = 0.01
+batch_size = 100
+hidden_layer_size = 256
+number_of_layers = 1
+dropout_rate = 0.5
+number_of_classes = 3
+gradient_clip_margin = 4
+window_size = 57
+
+model, session, restore_model, saver = None, None, None, None
+
+def slipt_data_part_1(data, rd_seed, train_ratio, max_train_size = float("inf")):
+    random.seed(seed = rd_seed)
+    train_length = min(max_train_size, int(train_ratio * len(data)))
+    print("Preparing train set...")
+    X_train, y_train = window_data(data[:train_length], window_size)
+    print("Preparing test set...")
+    X_test, y_test = window_data(data[train_length:], window_size)
+
+    return X_train, X_test, y_train, y_test
+
+
+def window_data(data, window_size):
+    X, y = [], []
+    
+    for s in tqdm_notebook(data):
+        i = 0
+        while (i + window_size) <= len(s) - 1:
+            X.append(s[i:i+window_size])
+            y.append(s[i+window_size])
+            i += 1
+
+    assert len(X) ==  len(y)
+    return np.array(X), np.array(y)
+
+
+def LSTM_cell(hidden_layer_size, batch_size, num_layers, dropout_rate = 0.5):
+    layer = tf.contrib.rnn.BasicLSTMCell(hidden_layer_size)
+    layer = tf.contrib.rnn.DropoutWrapper(layer, output_keep_prob = dropout_rate)                              
+    cell = tf.contrib.rnn.MultiRNNCell([layer] * num_layers)
+    init_state = cell.zero_state(batch_size, tf.float32)
+    return cell, init_state
+                      
+                                          
+def output_layer(lstm_output, in_size, out_size):
+    data = lstm_output[:, -1, :]
+    weights = tf.Variable(tf.truncated_normal([in_size, out_size], stddev = 0.05), name = 'output_layer_weights')
+    bias = tf.Variable(tf.zeros([out_size]), name = 'output_layer_bias')
+    output = tf.matmul(data, weights) + bias
+    return output
+                                          
+
+def optimize_loss(logits, targets, learning_rate, grad_clip_margin):
+    losses = []
+    for i in range(targets.get_shape()[0]):
+        losses.append([(tf.pow(logits[i] - targets[i], 2))])
+        
+    loss = tf.reduce_sum(losses)/(2*batch_size)
+    # Cliping gradient loss
+    gradients = tf.gradients(loss, tf.trainable_variables())
+    clipper_, _ = tf.clip_by_global_norm(gradients, grad_clip_margin)
+    optimizer = tf.train.AdamOptimizer(learning_rate)
+    train_optimizer = optimizer.apply_gradients(zip(gradients, tf.trainable_variables()))
+    return loss, train_optimizer
+
+                                          
+class recurrent_neural_net(object):
+    def __init__(self):
+        self.inputs = tf.placeholder(tf.float32, [batch_size, window_size, number_of_classes], name='input_data')
+        self.targets = tf.placeholder(tf.float32, [batch_size, number_of_classes], name='targets')
+        cell, init_state = LSTM_cell(hidden_layer_size, batch_size, number_of_layers, dropout_rate)
+        outputs, states = tf.nn.dynamic_rnn(cell, self.inputs, initial_state=init_state)
+        self.logits = output_layer(outputs, hidden_layer_size, number_of_classes)
+        self.loss, self.opt = optimize_loss(self.logits, self.targets, learning_rate, gradient_clip_margin)
+    
+    
+def prep_model(restore = None):
+    global model
+    global session
+    global restore_model
+    global saver
+    restore_model = restore
+    tf.reset_default_graph()
+    model = recurrent_neural_net()
+    session =  tf.Session()
+    session.run(tf.global_variables_initializer())
+    saver = tf.train.Saver()
+    
+    if not (restore_model):
+        saver.restore(session, "models/" + restore_model + ".ckpt")
+        print("Model restored.")
+
+        
+def train(X_train, y_train, name="foo"):
+
+    for i in tqdm_notebook(range(epochs)):
+        traind_scores = []
+        ii = 0
+        epoch_loss = []
+        pbar = tqdm_notebook(total = len(X_train)+1, leave=False)
+        while(ii + batch_size) <= len(X_train):
+            X_batch = X_train[ii:ii+batch_size]
+            y_batch = y_train[ii:ii+batch_size]
+            o, c, _ = session.run([model.logits, model.loss, model.opt], feed_dict={model.inputs:X_batch, model.targets:y_batch})
+            epoch_loss.append(c)
+            traind_scores.append(o)
+            ii += batch_size
+            pbar.update(batch_size)
+        pbar.close()
+        if (i % 10) == 0:
+            print('Epoch {}/{}'.format(i, epochs), ' Current loss: {}'.format(np.mean(epoch_loss)))
+        if (restore_model):
+            saver.save(session, 'models/' + name + '.ckpt')
+
+
+def test(input_): 
+    print("Creating new stroke...")
+    start = random.randint(0, high = len(input_) - 700)
+    X_test = input_[start:start + 700]
+    tests = []
+    i = 0
+    while i+batch_size <= 700:
+        o = session.run([model.logits], feed_dict={model.inputs:X_test[i:i+batch_size]})
+        i += batch_size
+        np.append(X_test, o[0])
+        tests.append(o)
+        
+    temp_test = (np.asarray(tests)).reshape(len(X_test),3)
+
+    std_data = np.std(temp_test[:, 0])
+    mean_data = np.mean(temp_test[:, 0])
+    
+    for count, item in enumerate(temp_test):
+            if item[0] <= mean_data + std_data:
+                temp_test[count][0] = int(0)
+            else:
+                temp_test[count][0] = int(1)
+                
+    return temp_test
